8828b4122e327894f086395dcb068b5e41406876
mwash-fred <mwangiwilly395@gmail.com>
2023-07-03 12:54:39 +0300

    How could you forget

--- src/main/java/apps/myuzinc/events/EventsApplication.java
+++ src/main/java/apps/myuzinc/events/EventsApplication.java
@@ -2,6 +2,7 @@ package apps.myuzinc.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -11,7 +12,8 @@ public class EventsApplication {
 		SpringApplication.run(EventsApplication.class, args);
 	}
 
-	public RestTemplate retTemplate(){
+	@Bean
+	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
 
